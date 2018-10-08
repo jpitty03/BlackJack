8a500fcb015692be62fe525cb53b02ef213f884c
jpitty <jpitty03@gmail.com>
2018-10-08 00:07:19 -0500

    to be continues

--- BlackJack/Program.cs
+++ BlackJack/Program.cs
@@ -10,27 +10,65 @@ namespace BlackJack
     {
         static void Main(string[] args)
         {
-            //ss
-            //sdsd
-            //send to github
-            //testing sync
-            //addison say hello
-            Console.WriteLine("Hello Birches");
-            for (int i = 0; i < 10; i++)
-            {
-                Console.WriteLine("i = {0}", i);
-            }
-            
+            int playerScore = 0, dealerScore = 0;
+            bool isPlaying, playerWin;
 
-            int x = 0;
+            Console.WriteLine("You wanna play some BJ? Y/N");
+            isPlaying = Console.ReadLine().ToString().Trim().ToLower() == "y";
+
+            while(isPlaying)
+            {
+                playerWin = PlayHand();
+                if(playerWin)
+                {
+                    playerScore++;
+                }
+                else
+                {
+                    dealerScore++;
+                }
+                isPlaying = WantsToPlay();
+
+            }
+
+
+        }
+
+
+
+        private static bool WantsToPlay()
+        {
+            Console.WriteLine("Wanna play? Y/N");
+            bool isPlaying = Console.ReadLine().ToString().Trim().ToLower() == "y";
+            Console.Clear();
+
+            return isPlaying;
+        }
+
+        private static bool PlayHand()
+        {
+            Random rng = new Random();
+            bool busted = false, beatDealer = false;
+            int playerHandValue = 0, dealerHandValue = 0, cardValue = 0;
 
             do
             {
-                Console.WriteLine("Fuck bitches get money");
-                x++;
+                playerHandValue = rng.Next(1, 11);
 
-            } while (x <= 69);
+
+
+
+                Console.WriteLine("you have hand of ");
+
+            } while (!busted || !beatDealer);
+
+            Console.WriteLine("yo playing hand");
             Console.ReadLine();
+            return false;
+
+
+            throw new NotImplementedException();
         }
     }
+
 }
